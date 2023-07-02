1e83dd7afaa7433bf70db47d34914b4146f10ff7
dep <1649743146@qq.com>
2023-07-02 20:01:19 +0800

    feat: h函数基本使用，以及h函数嵌套使用

--- 虚拟dom和diff/snabbdom-diff/src/index.js
+++ 虚拟dom和diff/snabbdom-diff/src/index.js
@@ -15,28 +15,17 @@ const patch = init([
   eventListenersModule, // attaches event listeners
 ]);
 
+const newVnode = h('a',{props: {href: 'http://www.baidu.com', target: '_blank'}}, '百度')
+
+// 中间的{}可以省略不写
+const newVnode2 = h('ul',{},[
+    h('li','我是第一个小li'),
+    h('li', [
+        h('span', '我是第二个li,我嵌套了一个span')
+      ]),
+    h('li','我是第三个小li')
+  ])
+console.log(newVnode2)
 const container = document.getElementById("container");
 
-const vnode = h("div#container.two.classes", { on: { click: () => console.log("div clicked") } }, [
-  h("span", { style: { fontWeight: "bold" } }, "This is bold"),
-  " and this is just normal text",
-  h("a", { props: { href: "/foo" } }, "I'll take you places!"),
-]);
-// Patch into empty DOM element – this modifies the DOM as a side effect
-patch(container, vnode);
-
-const newVnode = h(
-  "div#container.two.classes",
-  { on: { click: () => console.log("updated div clicked") } },
-  [
-    h(
-      "span",
-      { style: { fontWeight: "normal", fontStyle: "italic" } },
-      "This is now italic type"
-    ),
-    " and this is still just normal text",
-    h("a", { props: { href: "/bar" } }, "I'll take you places!"),
-  ]
-);
-// Second `patch` invocation
-patch(vnode, newVnode); // Snabbdom efficiently updates the old view to the new state
+patch(container, newVnode2); 
