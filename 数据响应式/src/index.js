66a1aa0d04f32397f79b1e5b169203b949c58904
dep <1649743146@qq.com>
2023-07-07 16:13:09 +0800

    feat: 数组响应式

--- 数据响应式/src/index.js
+++ 数据响应式/src/index.js
@@ -8,7 +8,10 @@ const obj = {
             }
         }
     },
-    text: 123
+    text: 123,
+    arr: [1, [2,3], {
+        name: 'zhangsan'
+    }]
 }
 observe(obj)
 console.log(obj.a.b)
@@ -19,3 +22,5 @@ obj.text = {
     }
 }
 console.log(obj.text.a.d)
+obj.arr.push(4)
+obj.arr.pop()
